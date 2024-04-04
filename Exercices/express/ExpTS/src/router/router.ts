ad4d2af25faa448645efafef83e0059230ebe053
Maaacs <msb@icomp.ufam.edu.br>
2024-04-03 22:53:56 -0400

    Exercicio 9

--- Exercices/express/ExpTS/src/router/router.ts
+++ Exercices/express/ExpTS/src/router/router.ts
@@ -1,73 +1,14 @@
 import { Router } from 'express';
-import { LoremIpsum } from 'lorem-ipsum';
-
+import mainController from '../controllers/main';
 const router = Router();
-const lorem = new LoremIpsum();
 
-router.get('/', (req, res) => {
-  res.send('Página principal do site');
-});
-
-router.get('/sobre', (req, res) => {
-  res.send('Página sobre');
-});
-
-
-router.get('/lorem/:num', (req, res) => {
-  const num = parseInt(req.params.num, 10);
-  let paragraphs = '';
-
-  for (let i = 0; i < num; i++) {
-    const numberOfSentences = Math.floor(Math.random() * (8 - 3 + 1)) + 3;
-    paragraphs += `<p>${lorem.generateSentences(numberOfSentences)}</p>`;
-  }
-
-  res.send(paragraphs);
-});
-
-
-router.get('/hb1', (req, res) => {
-    res.render('main/hb1', {
-        mensagem: 'Olá, você está aprendendo Express + HBS!',
-     layout: false,
-    });
-});
-
-router.get('/hb2', (req, res) => {
-    res.render('main/hb2', {
-        poweredByNodejs: true,
-        name: 'Express',
-        type: 'Framework',
-        layout: false,
-    });
-});
-
-
-router.get('/hb3', (req, res) => {
-    const profes = [ 
-    { nome: 'David Fernandes', sala: 1238 },
-    { nome: 'Horácio Fernandes', sala: 1233 },
-    { nome: 'Edleno Moura', sala: 1236 },
-    { nome: 'Elaine Harada', sala: 1231 }
-    ];
-    res.render('main/hb3', { profes, layout: false });
-});
-
-
-router.get('/hb4', (req, res) => {
-    const technologies = [
-      { name: 'Express', type: 'Framework', poweredByNodejs: true },
-      { name: 'Laravel', type: 'Framework', poweredByNodejs: false },
-      { name: 'React', type: 'Library', poweredByNodejs: true },
-      { name: 'Handlebars', type: 'Engine View', poweredByNodejs: true },
-      { name: 'Django', type: 'Framework', poweredByNodejs: false },
-      { name: 'Docker', type: 'Virtualization', poweredByNodejs: false },
-      { name: 'Sequelize', type: 'ORM tool', poweredByNodejs: true },
-    ];
-  
-    res.render('main/hb4', { technologies });
-});
-  
+router.get('/', mainController.inicial);
+router.get('/sobre', mainController.sobre);
+router.get('/lorem', mainController.lorem);
+router.get('/hb1', mainController.hb1);
+router.get('/hb2', mainController.hb2);
+router.get('/hb3', mainController.hb3);
+router.get('/hb4', mainController.hb4);
 
 
 export default router;
