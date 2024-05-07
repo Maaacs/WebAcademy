c05e83ea4eba163bf8a6043ab8a1c457ba61a69b
Maaacs <msb@icomp.ufam.edu.br>
2024-05-07 18:51:00 -0400

    colapsar

--- Exercices/react/loja/src/app/layout.tsx
+++ Exercices/react/loja/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar } from "./components/Navbar";
+import BootstrapClient from "./components/BootstrapClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body>
         <Navbar />  
         {children}
+        <BootstrapClient />
       </body>
     </html>
   );
