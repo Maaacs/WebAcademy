cd695214de632b048d94ae840296dea6954d918f
Maaacs <msb@icomp.ufam.edu.br>
2024-05-15 19:36:38 -0400

    +,- e remover funcionando. Quantidade total e valor em resumo nas duas páginas funcionando também.

--- Exercices/react/loja/src/app/hooks/useDetalheProdutos.ts
+++
@@ -1,6 +0,0 @@
-/*import { useQuery } from "react-query";
-import { getDetalhesProduto } from "../services/produtos";
-
-export function useDetalhesProduto(produto: string) {
-  return useQuery(["produto", produto], () => getDetalhesProduto(produto));
-}*/
--- Exercices/react/loja/src/app/services/produtos.ts
+++
@@ -1,8 +0,0 @@
-/*import axios from "axios";
-
-export async function getDetalhesProduto(produto: string) {
-  const { data } = await axios.get(
-    `https://ranekapi.origamid.dev/json/api/produto/${produto}`
-  );
-  return data;
-}*/
